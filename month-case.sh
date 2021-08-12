6fb4329459d76699435d1703653ec5f0cd44ec94
suba <root@ip-172-31-22-94.ap-south-1.compute.internal>
2021-08-12 11:49:20 +0000

    Adding month case

---
+++ month-case.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "You entered month number : $num" 
+case [ $num ] in
+        0)
+                echo "January" ;;
+        1)
+		echo "February" ;;
+        2)
+		echo "March" ;;
+
+       3)
+		echo "April" ;;
+	4)
+		echo "May" ;;
+	5)
+		echo "June" ;;
+	6)
+		echo "July" ;;
+	7)
+		echo "August" ;;
+	8)
+		echo "September" ;;
+	9)
+		echo "October" ;;
+	10)
+		echo "November" ;;
+	11)
+		echo "December" ;;
+esac
+
+
