e6335779832c3082234f2dac92e0eb60a585ed81
suba <root@ip-172-31-22-94.ap-south-1.compute.internal>
2021-08-12 09:42:14 +0000

    Adding backup for jenkins workspace

--- backup-jenkins-home.sh
+++ backup-jenkins-home.sh
@@ -3,4 +3,4 @@
 DATE=$(date +%S-%M-%H-%d-%m-%Y)
 BACKUP_DIR=/var/lib/jenkins/backup
 
-tar -czvf $BACKUP_DIR/jenkins-home-$DATE.tar.gz /var/lib/jenkins/
+tar -czvf $BACKUP_DIR/jenkins-home-$DATE.tar.gz /var/lib/jenkins/workspace/
