64904299c66010dbfccff323ba82e5bc01a0476a
suba <root@ip-172-31-22-94.ap-south-1.compute.internal>
2021-08-14 14:44:15 +0000

    Adding backup

--- backup-script.sh
+++ backup-script.sh
@@ -3,4 +3,4 @@
 DATE=$(date +%S-%M-%H-%d-%m-%Y)
 BACKUP_DIR=/var/log/backup
 
-tar -czvf $BACKUP_DIR/backup-script-$DATE.tar.gz /var/log/jenkins/
+tar -czvf $BACKUP_DIR/backup-script-$DATE.tar.gz /var/log/jenkins
